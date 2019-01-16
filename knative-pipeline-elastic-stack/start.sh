2eac1a7aae61c749fe5926a1735c0122610c5087
Maxime Gréau <maxime.greau@elastic.co>
2019-01-16 09:48:20 +0100

    running

--- knative-pipeline-elastic-stack/start.sh
+++ knative-pipeline-elastic-stack/start.sh
@@ -2,14 +2,9 @@
 set -e
 
 # Start K8s  
-launch.sh
+#launch.sh
 
-# Deploy kube-state-metrics
-kubectl apply -f  https://raw.githubusercontent.com/mgreau/katacoda-scenarios/master/knative-pipeline-elastic-stack/assets/k8s/kube-state-metrics.yaml
-
-# Download Elastic docker images
-TAG=6.5.4
-docker pull docker.elastic.co/elasticsearch/elasticsearch:${TAG}
-#docker pull docker.elastic.co/kibana/kibana:${TAG}
-#docker pull docker.elastic.co/beats/filebeat:${TAG}
-#docker pull docker.elastic.co/beats/metricbeat:${TAG}
+# Create files for tutorial
+mkdir -p hello-world-task
+touch hello-world-task/01-task.yaml
+touch hello-world-task/02-task-run.yaml
