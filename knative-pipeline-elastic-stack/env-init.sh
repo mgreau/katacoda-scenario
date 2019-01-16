29aecb4b2ae5fd3d073cb66d0122ae8ef3a9db5f
Maxime Gréau <maxime.greau@elastic.co>
2019-01-16 08:53:55 +0100

    Start minikube

--- knative-pipeline-elastic-stack/env-init.sh
+++ knative-pipeline-elastic-stack/env-init.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -e
 
-# Start Kubernetes
-launch.sh
+# Start Minikube  
+minikube start
 
 # Deploy kube-state-metrics
 kubectl apply -f  https://raw.githubusercontent.com/mgreau/katacoda-scenarios/master/knative-pipeline-elastic-stack/assets/k8s/kube-state-metrics.yaml
-
-# Create files for tutorial
-mkdir -p hello-world-task
-touch hello-world-task/01-task.yaml
-touch hello-world-task/02-task-run.yaml
 
 # Download Elastic docker images
 TAG=6.5.4
---
+++ knative-pipeline-elastic-stack/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+# Create files for tutorial
+mkdir -p hello-world-task
+touch hello-world-task/01-task.yaml
+touch hello-world-task/02-task-run.yaml
